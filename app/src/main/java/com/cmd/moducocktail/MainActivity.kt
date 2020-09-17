d478656f79e06cb992886b7268db574fba09b18c
KangJunewoo <brianjune.dev@gmail.com>
2020-09-17 13:32:28 +0900

    [DELETE] 기본 액티비티 전환코드 삭제

--- app/src/main/java/com/cmd/moducocktail/MainActivity.kt
+++ app/src/main/java/com/cmd/moducocktail/MainActivity.kt
@@ -11,24 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, SubActivity::class.java)
-        intent.putExtra("from1", "hello bundle")
-        intent.putExtra("from2", 2020)
-
-        button.setOnClickListener{startActivityForResult(intent, 99)}
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(resultCode == Activity.RESULT_OK){
-            when(requestCode){
-                99 -> {
-                    val message = data?.getStringExtra("returnValue")
-                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-                }
-            }
-        }
     }
 }
