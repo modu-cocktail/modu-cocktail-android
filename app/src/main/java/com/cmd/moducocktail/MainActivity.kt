958aa78ba94b03562389f86628136af67ab2f20b
KangJunewoo <brianjune.dev@gmail.com>
2020-09-18 10:42:13 +0900

    [DELETE] 기존 코드 초기화

--- app/src/main/java/com/cmd/moducocktail/MainActivity.kt
+++ app/src/main/java/com/cmd/moducocktail/MainActivity.kt
@@ -16,23 +16,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val data:MutableList<Memo> = loadData()
-        var adapter = CustomAdapter()
-        adapter.listData = data
-        recyclerView.adapter = adapter
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-    }
-
-    fun loadData() : MutableList<Memo>{
-        val data:MutableList<Memo> = mutableListOf()
-        for(no in 0..100){
-            val title = "이것이 코틀린이다 ${no+1}"
-            val date = System.currentTimeMillis()
-            var memo = Memo(no, title, date)
-            data .add(memo)
-        }
-        return data
     }
 }
