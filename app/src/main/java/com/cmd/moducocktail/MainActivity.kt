cee061cf2a6c6ee0bfd47d60474ee01cc7147d95
KangJunewoo <brianjune.dev@gmail.com>
2020-09-23 15:23:05 +0900

    [ADD] 리싸이클러뷰 주석 & 상단 탭 구현

--- app/src/main/java/com/cmd/moducocktail/MainActivity.kt
+++ app/src/main/java/com/cmd/moducocktail/MainActivity.kt
@@ -9,30 +9,42 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data:MutableList<Memo> = loadData()
-        var adapter = CustomAdapter()
-        adapter.listData = data
-        recyclerView.adapter = adapter
+        val fragmentList = listOf(HomeFragment(), RecommendationFragment(), MyFragment())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        viewPager.adapter = adapter
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
+        val tabTitles = listOf<String>("홈", "추천", "My")
+        TabLayoutMediator(tabLayout, viewPager){tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
+
+//        val data:MutableList<Memo> = loadData()
+//        var adapter = CustomAdapter()
+//        adapter.listData = data
+//        recyclerView.adapter = adapter
+//
+//        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
-    fun loadData() : MutableList<Memo>{
-        val data:MutableList<Memo> = mutableListOf()
-        for(no in 0..100){
-            val title = "이것이 코틀린이다 ${no+1}"
-            val date = System.currentTimeMillis()
-            var memo = Memo(no, title, date)
-            data .add(memo)
-        }
-        return data
-    }
+//    fun loadData() : MutableList<Memo>{
+//        val data:MutableList<Memo> = mutableListOf()
+//        for(no in 0..100){
+//            val title = "이것이 코틀린이다 ${no+1}"
+//            val date = System.currentTimeMillis()
+//            var memo = Memo(no, title, date)
+//            data.add(memo)
+//        }
+//        return data
+//    }
 }
