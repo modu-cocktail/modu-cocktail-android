b169c3ebaa7b0e0ead2f83a967a68028072f8450
KangJunewoo <brianjune.dev@gmail.com>
2020-09-23 14:53:49 +0900

    [ADD] TabLayout + ViewPager2 사용해 스와이프 가능한 상단메뉴 구현

--- app/src/main/java/com/cmd/moducocktail/MainActivity.kt
+++ app/src/main/java/com/cmd/moducocktail/MainActivity.kt
@@ -9,12 +9,24 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentList = listOf(FragmentA(), FragmentB(), FragmentC(), FragmentD())
+        val adapter = FragmentAdapter(this)
+        adapter.fragmentList = fragmentList
+        viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("A", "B", "C", "D")
+        TabLayoutMediator(tabLayout, viewPager){tab, position->
+            tab.text = tabTitles[position]
+        }.attach()
     }
 }
