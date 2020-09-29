6f6aa411c932f744cb2598ae1f5a98bc26936fc4
KangJunewoo <brianjune.dev@gmail.com>
2020-09-29 16:58:45 +0900

    [ADD] 모칵 스플래시 스크린 적용

--- app/src/main/java/com/cmd/moducocktail/SplashActivity.kt
+++ app/src/main/java/com/cmd/moducocktail/SplashActivity.kt
@@ -1,11 +1,20 @@
 package com.cmd.moducocktail
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
+    // 스플래시 로딩 시간
+    private val SPLASH_TIME_OUT:Long = 3000
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
     }
 }
