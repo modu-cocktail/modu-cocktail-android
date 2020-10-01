7722e31c8d4e4b9b6388396fa41ff42d4edd56b3
KangJunewoo <brianjune.dev@gmail.com>
2020-10-01 12:01:20 +0900

    [ADD] 리싸이클러뷰 클릭시 인텐트 넘어감(칵테일 번들에 추가 필요)

--- app/src/main/java/com/cmd/moducocktail/CocktailAdapter.kt
+++ app/src/main/java/com/cmd/moducocktail/CocktailAdapter.kt
@@ -1,17 +1,32 @@
 package com.cmd.moducocktail
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 
 class CocktailAdapter(val cocktailList: ArrayList<Cocktails>) : RecyclerView.Adapter<CocktailAdapter.CustomViewHolder>(){
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CocktailAdapter.CustomViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.list_cocktail, parent, false)
         // TODO : 클릭 시 넘어가는 인텐트 추가
-        return CustomViewHolder(view)
+
+
+        return CustomViewHolder(view).apply{
+            itemView.setOnClickListener{
+                val curPos: Int = adapterPosition
+                val cocktail: Cocktails = cocktailList[curPos]
+
+                // 인텐트 번들에 넣어 실행하기
+                val intent = Intent(itemView?.context, DetailActivity::class.java)
+                // TODO : intent.putExtra("cocktail", cocktail) 왜 오류뜨지?
+                ContextCompat.startActivity(itemView.context, intent, null)
+
+            }
+        }
     }
 
     override fun getItemCount(): Int {
@@ -29,3 +44,4 @@ class CocktailAdapter(val cocktailList: ArrayList<Cocktails>) : RecyclerView.Ada
         val name = itemView.findViewById<TextView>(R.id.cocktail_name)
     }
 }
+
