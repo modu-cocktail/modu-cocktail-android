010761983c586ccab8ca33bb89b5eac4b935a264
KangJunewoo <brianjune.dev@gmail.com>
2020-10-08 21:08:24 +0900

    [ADD] 토글버튼 간단한 로직 추가

--- app/src/main/java/com/cmd/moducocktail/RecommendationFragment.kt
+++ app/src/main/java/com/cmd/moducocktail/RecommendationFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ToggleButton
+import kotlinx.android.synthetic.main.fragment_recommendation.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,7 +29,7 @@ class RecommendationFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
+//        val tb_spriteYes: ToggleButton = findvi
 
     }
 
@@ -39,6 +41,35 @@ class RecommendationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_recommendation, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // TODO : 토글버튼에 text 대신 이미지 적용
+        tb_spriteYes.textOn = "O"
+        tb_spriteYes.textOff = "O"
+        tb_spriteNo.textOn = "X"
+        tb_spriteNo.textOff = "X"
+        tb_alcoholHigh.textOn = "⬆️"
+        tb_alcoholHigh.textOff = "⬆️"
+        tb_alcoholMid.textOn = "↔️️"
+        tb_alcoholMid.textOff = "↔️️"
+        tb_alcoholLow.textOn = "⬇️"
+        tb_alcoholLow.textOff = "⬇️"
+        tb_spriteYes.toggle()
+        tb_spriteYes.toggle()
+        tb_spriteNo.toggle()
+        tb_spriteNo.toggle()
+        tb_alcoholHigh.toggle()
+        tb_alcoholHigh.toggle()
+        tb_alcoholMid.toggle()
+        tb_alcoholMid.toggle()
+        tb_alcoholLow.toggle()
+        tb_alcoholLow.toggle()
+        tb_spriteYes.setOnCheckedChangeListener { _, isChecked ->
+//            tb_spriteNo.toggle()
+        }
+
+
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
