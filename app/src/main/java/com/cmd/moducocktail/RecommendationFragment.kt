db4f729c2e4e4e7fa122f6df0d20a9e855ff2362
KangJunewoo <brianjune.dev@gmail.com>
2020-10-08 20:50:47 +0900

    [ADD] 토글버튼 id값 추가

--- app/src/main/java/com/cmd/moducocktail/RecommendationFragment.kt
+++ app/src/main/java/com/cmd/moducocktail/RecommendationFragment.kt
@@ -27,6 +27,8 @@ class RecommendationFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
